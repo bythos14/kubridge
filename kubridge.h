b7aa6fb4c44d05b62f69321606ecfc8e2b78848b
bythos14 <45720552+bythos14@users.noreply.github.com>
2022-08-21 18:44:56 -0400

    Add MemDecommit function
    MemDecommit is the counterpart to MemCommit. It will free phypages in a range, or simply unmap pages that were commited as a mirror of another memBlock.
    
    Also made adjustments to page mapping and un-mapping done by MemProtect

--- kubridge.h
+++ kubridge.h
@@ -108,9 +108,10 @@ int kuPowerSetSysClockFrequency(int freq);
 int kuKernelRegisterAbortHandler(KuKernelAbortHandler pHandler, KuKernelAbortHandler *pOldHandler, KuKernelAbortHandlerOpt *pOpt);
 void kuKernelReleaseAbortHandler();
 
-int kuKernelMemProtect(void *addr, SceSize size, SceUInt32 prot);
+int kuKernelMemProtect(void *addr, SceSize len, SceUInt32 prot);
 SceUID kuKernelMemReserve(void **addr, SceSize size, SceKernelMemBlockType memBlockType);
 int kuKernelMemCommit(void *addr, SceSize len, SceUInt32 prot, KuKernelMemCommitOpt *pOpt);
+int kuKernelMemDecommit(void *addr, SceSize len);
 
 #ifdef __cplusplus
 }
